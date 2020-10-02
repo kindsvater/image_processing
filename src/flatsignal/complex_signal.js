083cfa656d8fac18a0447fa71bac9a61394ae62f
jkinsfat <jasonrkinsfather@gmail.com>
2020-10-02 12:46:16 -0700

    Add JSDoc comments to complex_signal.js

--- src/flatsignal/complex_signal.js
+++ src/flatsignal/complex_signal.js
@@ -1,12 +1,20 @@
 const { mag } = require('../utility/linearalg_util.js');
 const { bankRound } = require('../utility/num_util.js');
-
-//Returns the magnitude of the complex sinusoid
+/**
+ * Calculates the magnitude of a sinusoid represented by a complex number.
+ * @param {Number} re The Real Component 
+ * @param {Number} im The Imaginary component
+ * @returns The magnitude of the sinusoid.
+ */
 function magnitude(re, im) {
     return mag([re, im]);
 }
-
-//Returns the phase of the complex sinusoid in Radians
+/**
+ * Calculates the phase of a sinusoid represented by a complex number. 
+ * @param {Number} re The Real Component
+ * @param {Number} im The Imaginary Component
+ * @returns {Number} The phase in Radians
+ */
 function phase(re, im) {
     let theta;
     //Avoid divide by Zero error when Real Part is 0
@@ -25,19 +33,30 @@ function phase(re, im) {
 
     return theta;
 }
-
-//Converts complex sinusoid in rectangular form to polar form. Phase is in Radians.
+/**
+ * Converts a Rectangular representation of a complex sinusoid to Polar form.
+ * @param {Number} re The Real Component of Rectangular Form
+ * @param {Number} im The Imaginary Component of Rectangular Form
+ * @returns {Array.<Number>} An array with the Polar magnitude and phase: [magnitude, phase]
+ */
 function rectToPolar(re, im) {
     return [magnitude(re, im), phase(re, im)];
 }
-
-//Converts complex sinusoid in polar form to rectangular form. 
-function polarToRect(m, p) {
-    return [m * Math.cos(p), m * Math.sin(p)];
+/**
+ * Converts complex sinusoid in polar form to rectangular form. 
+ * @param {Number} magnitude 
+ * @param {Number} phase 
+ * @returns {Array.<Number>} An array with the Rectangular Real and Imaginary Components: [Real, Imaginary]
+ */
+function polarToRect(magnitude, phase) {
+    return [magnitude * Math.cos(phase), magnitude * Math.sin(phase)];
 }
-
-//Converts frequency domain signal represented by two complex sinusoid components ReX and ImX into 
-//polar representation with components MagX and PhaseX.
+/**
+ * Converts n-length signal in the frequency domain, represented in Rectangular Form, to Polar Form.
+ * @param {Array.<Number>} ReX n-length array of Real Components 
+ * @param {Array.<Number>} ImX n-length array of Imaginary Components
+ * @returns {Array.<Array.<Number>>} An array with the n-length magnitude and phase components: [[Magnitude], [Phase]]
+ */
 function polarSignal(ReX, ImX) {
     let MagX = [],
         PhaseX = [];
@@ -49,9 +68,12 @@ function polarSignal(ReX, ImX) {
 
     return [MagX, PhaseX];
 }
-
-//Converts frequency domain signal represented by polar components MagX and PhaseX into 
-//complex sinusoidal representation with components ReX and ImX.
+/**
+ * Converts n-length signal in the frequency domain, represented in Polar Form, to Rectangular Form.
+ * @param {Array.<Number>} MagX n-length array of Magnitude Components
+ * @param {Array.<Number>} PhaseX n-length array of Phase Components
+ * @returns {Array.<Array.<Number>>} An array with both n-length Real and Imaginary Components: [[Real], [Imaginary]]
+ */
 function rectSignal(MagX, PhaseX) {
     let ReX = [],
         ImX = [];
@@ -62,10 +84,12 @@ function rectSignal(MagX, PhaseX) {
     }
     return [ReX, ImX];
 }
-
-//If the phase component of a signal has too many discontinuities and is hard to visualize,
-//unwrap the phase by adding or subtracting 2PI from each value based on the minimum difference 
-//between adjacent samples. 
+/**
+ * Unwraps the phase value by adding or subtracting 2PI from each value based on the minimum difference 
+ * between adjacent samples. Use when the phase component has too many discontinuities and is hard to visualized.
+ * @param {Array.<Number>} phaseX n-length array of Phase Components
+ * @returns {Array.<Number>} An n-length array of unwrapped Phase Components.
+ */
 function unwrapPhase(phaseX) {
     let uwPhase = [0];
 
